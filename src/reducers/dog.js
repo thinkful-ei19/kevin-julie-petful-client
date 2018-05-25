5bf233c5b27e423567efa4fdc384b4271b49e7fc
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-25 11:41:17 -0700

    dog reducers

--- src/reducers/dog.js
+++ src/reducers/dog.js
@@ -1,17 +1,44 @@
 const initialState = {
   loading: false,
   error: null,
-  pets: [],
+  pets: null,
 };
 
-export function postReducer(state = initialState, action) {
+export default function dogReducer(state = initialState, action) {
   switch (action.type) {
-    case '':
-      return {
-        ...state,
+    case 'FETCH_DOG_REQUEST':
+      return Object.assign({}, state, {
+        error: null,
         loading: true,
-      };
+      });
+    case 'FETCH_DOG_SUCCESS':
+      return Object.assign({}, state, {
+        pets: action.payload,
+        loading: false,
+      });
+    case 'FETCH_DOG_ERROR':
+      return Object.assign({}, state, {
+        loading: false,
+        error: action.payload,
+      });
+    case 'ADOPT_DOG_REQUEST':
+      return Object.assign({}, state, {
+        error: null,
+        loading: true,
+      });
+    case 'ADOPT_DOG_SUCCESS':
+      return Object.assign({}, state, {
+        pets: null,
+        error: null,
+        loading: false,
+      });
+    case 'ADOPT_DOG_ERROR':
+      return Object.assign({}, state, {
+        error: action.payload,
+        loading: false,
+      });
     default:
       return state;
   }
+  return state;
 }
