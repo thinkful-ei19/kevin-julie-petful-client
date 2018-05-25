acc3264256607542c28e18ac2d5a58b65e890212
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-25 10:30:03 -0700

    updated file structure

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Pet extends Component {
+  render() {
+    return (
+
+      <header className="App-header">
+        <h1 className="App-title">Petful</h1>
+        <p className="App-intro">
+          Pet adoption service
+          </p>
+      </header>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
+import Dashboard from './Dashboard';
+import Header from './components/Header'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div className="App">
+    <Header />
+    <Dashboard />
+  </div>,
+  document.getElementById('root'))
 registerServiceWorker();
